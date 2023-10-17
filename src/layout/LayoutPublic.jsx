fcaa05230a7436cb024ba82be73829dc70b62fdb
Mario Rodriguez Martinez <mariog.rodriguezm@gmail.com>
2023-10-17 19:18:01 +0200

    first commit

---
+++ src/layout/LayoutPublic.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+const LayoutPublic = () => {
+  return (
+    <>
+     <Navbar/>
+      <main className="container">
+        <Outlet/>
+      </main>
+      <footer className="text-center">Footer</footer>
+    </>
+  );
+};
+export default LayoutPublic;
